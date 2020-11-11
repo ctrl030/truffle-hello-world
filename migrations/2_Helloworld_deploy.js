ae9403e482691768741acb8537389b7e34d6f4da
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-11 19:36:20 -0300

    Update 2_Helloworld_deploy.js

--- migrations/2_Helloworld_deploy.js
+++ migrations/2_Helloworld_deploy.js
@@ -1,6 +1,6 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
   deployer.deploy(HelloWorld).then(function(instance){
     instance.setMessage("Hey there!", {value: 100000000000000000, from: accounts[0]}).then(function(){
       console.log("Success");
